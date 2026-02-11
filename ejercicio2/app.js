95135253caf299a071d750e0972d4d0d540d1bea
michaelbenavides3 <michaelfb803@gmail.com>
2026-02-11 17:18:01 -0500

    comentadocodigodeapp.js

--- ejercicio2/app.js
+++ ejercicio2/app.js
@@ -1,10 +1,13 @@
 
+//importamos la libreria para preguntarle por medio de la terminar (para pedir datos al usuario)
 import promptSync from 'prompt-sync';
 import {calcularNota} from "./index.js";
 
+//inicilaimzo promptsync y se guarda en una constante
 const prompt = promptSync();
 
 //pedimos las notas al usuario
+
 
 const n1 = parseFloat(prompt("ingresar nota 1: "));
 const n2 = parseFloat(prompt("ingresar nota 2: "));
